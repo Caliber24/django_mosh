ac5c487e5bf6939209008f923455591ce53c5306
Caliber24 <amirhosseinpishroa2001@gmail.com>
2024-09-24 16:31:12 +0330

    adding productImage inline

--- core/admin.py
+++ core/admin.py
@@ -2,29 +2,32 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.models import Product
-from store.admin import ProductAdmin
+from store.admin import ProductAdmin, ProductImageInline
 from tags.models import TaggedItem
 from .models import User
 # Register your models here.
 
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-  add_fieldsets = (
-          (
-              None,
-              {
-                  "classes": ("wide",),
-                  "fields": ("username", "password1", "password2", 'email','first_name', 'last_name'),
-              },
-          ),
-      )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", 'email', 'first_name', 'last_name'),
+            },
+        ),
+    )
+
 
 class TagInline(GenericTabularInline):
-    autocomplete_fields=['tag']
+    autocomplete_fields = ['tag']
     model = TaggedItem
 
+
 class CustomProductAdmin(ProductAdmin):
-  inlines = [TagInline]
+    inlines = [TagInline, ProductImageInline]
 
 
 admin.site.unregister(Product)
