d235d4f9024a8908ab61af13c8d09144274aa9ad
Caliber24 <amirhosseinpishroa2001@gmail.com>
2024-09-24 16:33:04 +0330

    create validate for image

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
+from .validators import validate_file_size
 from django.contrib import admin
 from uuid import uuid4
 
@@ -44,6 +45,10 @@ class Product(models.Model):
     
     class Meta:
         ordering=['title']
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models. ImageField(upload_to='store/images', validators=[validate_file_size])
 
 
 class Customer(models.Model):
